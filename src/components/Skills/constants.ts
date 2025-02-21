a0db0efe64774627aece138a82d2753598c7f332
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-02-21 20:08:33 +0300

    add skills section

---
+++ src/components/Skills/constants.ts
@@ -0,0 +1,17 @@
+export const skills = [
+  {
+    id: "js",
+    title: "JavaScript",
+    description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id: "reactjs",
+    title: "React.JS",
+    description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id: "nodejs",
+    title: "Node.JS",
+    description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+]
